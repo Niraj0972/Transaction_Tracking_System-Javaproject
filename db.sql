c61412c0db5c631f3f9bdbc8477b0f3fc73cb27e
Niraj0972 <birariniraj@gmail.com>
2025-07-06 21:56:42 +0530

    Delete db.sql

--- db.sql
+++
@@ -1,8 +0,0 @@
-create database Student;
-USE Student;
-CREATE TABLE Student(ID INT Primary Key, Name varchar(50),Marks varchar(20));
-INSERT INTO Student(ID, Name, Marks) VALUES ('101' ,"Niraj",'80');
-SELECT *   FROM INFO;
-INSERT INTO Student(ID, Name, Marks) VALUES ('102' ,"Umesh",'75'),(103, "Akshay", '78');
-INSERT INTO Student(ID, Name, Marks) VALUES ('104' ,"Ashwini",'70'),(105, "Jayesh", '84'); 
-INSERT INTO Student(ID, Name, Marks)values('106',"YASH",'86');
